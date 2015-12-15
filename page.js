456f17cc48012d74d8e92f48335e04bfa9a52a65
Renaud Vivancos <renaud.vivancos@free.fr>
2015-12-15 18:46:46 +0100

    ajout du css, mais il reste encore des soucis sur les menus et un peu de css a faire

--- page.js
+++ page.js
@@ -1,24 +1,42 @@
 $(document).ready(function() {
    
-    $('li').hide();
-    $('#menu1').click(function() {
-    	$('#menu1 li').toggle();
+    $('ul li ul  li').css('visibility', 'hidden');
+
+    $('#menu1').mouseover(function() {
+    	$('#menu1').css('backgroundColor', 'blue');
+    	$('#menu1 li').css('visibility', 'visible');
+    });
+    $('#menu1').mouseout(function() {
+    	$('#menu1').css('backgroundColor', '');
+    	$('#menu1 li').css('visibility', 'hidden');
     });
 
- 	$('li').hide();
-    $('#menu2').click(function() {
-    	$('#menu2 li').toggle();
+	$('#menu2').mouseover(function() {
+    	$('#menu2').css('backgroundColor', 'blue');
+    	$('#menu2 li').css('visibility', 'visible');
     });
-
-     $('li').hide();
-    $('#menu3').click(function() {
-    	$('#menu3 li').toggle();
+    $('#menu2').mouseout(function() {
+    	$('#menu2').css('backgroundColor', '');
+    	$('#menu2 li').css('visibility', 'hidden');
     });
-
-    $('li').hide();
-    $('#menu4').click(function() {
-    	$('#menu4 li').toggle();
+    
+    $('#menu3').mouseover(function() {
+    	$('#menu3').css('backgroundColor', 'blue');
+    	$('#menu3 li').css('visibility', 'visible');
     });
-
+    $('#menu3').mouseout(function() {
+    	$('#menu3').css('backgroundColor', '');
+    	$('#menu3 li').css('visibility', 'hidden');
+    });
+    
+    $('#menu4').mouseover(function() {
+    	$('#menu4').css('backgroundColor', 'blue');
+    	$('#menu4 li').css('visibility', 'visible');
+    });
+    $('#menu4').mouseout(function() {
+    	$('#menu4').css('backgroundColor', '');
+    	$('#menu4 li').css('visibility', 'hidden');
+    });
+	
     new IdealImageSlider.Slider('#diaporama');
 });
